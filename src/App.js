b3d9e1159f7988df1a94a7a09c9425d6a4fb9e40
DESKTOP-94GR966\weaver-gram-0011 <ksh0521@weaverloft.com>
2022-12-20 17:27:44 +0900

    함수형 컴포넌트 기초(커피) 221220

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+function Coffee({fav, say}) {
+    return (
+        <div>
+            <h1>{fav}는 말이야!</h1>
+            <p>{say}</p>
+        </div>
+    );
+}
+
+const CoffeeList = [
+    {
+        id: 1,
+        name : "Latte",
+        comment : "라떼는 1이야",
+        rating: 5
+    },
+    {
+        id: 2,
+        name: "Americano",
+        comment: "아메리카노는 2야",
+        rating: 4.7
+    },
+    {
+        id: 3,
+        name: "Cappuccino",
+        comment: "카푸치노는 3이야",
+        rating: 3.2
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>연습입니다. 안녕하세요</p>
+            {CoffeeList.map(menu => <Coffee key={menu.id} fav={menu.name} say={menu.comment}/>)}
+        </div>
+    );
 }
+
+
 
 export default App;
